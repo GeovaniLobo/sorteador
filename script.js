c43487f0fb22bcdf17eddc467b45619c57027180
GeovaniLobo <geovanilobo98@gmail.com>
2023-08-01 14:38:02 -0300

    alteração no js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generateNumber() {
 
-const min = document.querySelector('input-min').value
-const max = document.querySelector('input-max').value
+const min = Math.ceil(document.querySelector('input-min').value)
+const max = Math.floor(document.querySelector('input-max').value)
 
 const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
